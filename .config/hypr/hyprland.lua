336d81d5c2095684240b71601ee20f8de9ce105f
Hugo Coto <hugocoto100305@gmail.com>
2026-06-25 14:11:42 +0200

    Adding international layout and es on win-space

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -193,10 +193,10 @@ hl.config({
 
 hl.config({
     input = {
-        kb_layout    = "us",
-        kb_variant   = "",
+        kb_layout    = "us,es",
+        kb_variant   = "intl",
         kb_model     = "",
-        kb_options   = "",
+        kb_options   = "grp:win_space_toggle",
         kb_rules     = "",
 
         follow_mouse = 1,
