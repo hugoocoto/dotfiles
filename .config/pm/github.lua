41fd49396c819df8ad3ff91f75059212dba2ee15
Hugo Coto <hugocoto100305@gmail.com>
2026-07-10 01:13:35 +0200

    Update pm comfig to new pm api

--- .config/pm/github.lua
+++ .config/pm/github.lua
@@ -2,7 +2,8 @@ function Github_make(user, repo, branch, artifact)
     return {
         url = "https://github.com/" .. user .. "/" .. repo .. "/archive/refs/heads/" .. branch .. ".tar.gz",
         name = repo .. ".tar.gz",
-        build = "tar -xzf " .. repo .. ".tar.gz && mv " .. repo .. "-" .. branch .. "/* . && make",
+        last_modified_cmd = "curl -s https://api.github.com/repos/" .. user .. "/" .. repo .. "/commits/" .. branch .. " -I | grep -i last-modified",
+        build = "tar -xzf " .. repo .. ".tar.gz && cp -fr " .. repo .. "-" .. branch .. "/* . && make",
         artifact = artifact,
     }
 end
