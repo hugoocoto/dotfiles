7e8deffc9dd926ee459a22d64ad8e7a974109d19
Hugo Coto <hugocoto100305@gmail.com>
2026-07-19 11:28:26 +0200

    rename this one

--- .local/bin/sysupdate.sh
+++ .local/bin/sysupdate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # update mirrorlist.
+echo "Updating mirrorlists"
 sudo reflector --country France --fastest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 # update the system.
+echo "Updating the system"
 sudo pacman -Syyu
